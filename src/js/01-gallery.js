abb3a3af0f4c6acdd434012162799a2382f1fffc
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-17 18:27:10 +0200

    simplelightbox npm installed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+
+const makeGalleryContainerMarkup = galleryItems.map((image) => {
+    const { preview, original, description } = image;
+    return `
+        <li class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"
+                />
+            </a>
+        </li>`;
+}).join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", makeGalleryContainerMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
+
+console.log(galleryItems);
+
 
 console.log(galleryItems);
