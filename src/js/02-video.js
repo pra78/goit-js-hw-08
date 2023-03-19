c0c36dba973d8a47d6ea11c5154fae4459c0c06d
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-19 13:53:45 +0200

    task 2 remade without promises

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const iframePlayer = new Player(iframe);
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
 
-iframePlayer.on('timeupdate', throttle(() => {
-    const currentTime = iframePlayer.getCurrentTime().then(function (seconds) {
-        // seconds = the current playback position
-        localStorage.setItem("videoplayer-current-time", seconds);
-    }).catch(function (error) {
-        // an error occurred
-        console.log(error);
-    })
-}, 1000));
+player.on('timeupdate', throttle(savePlayingTime, 1000));
+
+function savePlayingTime(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
 
 const savedTime = localStorage.getItem("videoplayer-current-time");
 
 if (savedTime) {
-    iframePlayer.setCurrentTime(savedTime);
+    player.setCurrentTime(savedTime);
 }
